32be9a7b82a456e238d886ff4a36dcacf082e7b1
Kulshresth Jangid <kulshresthjangid@gmail.com>
2024-02-28 00:06:13 +0530

    Added controller for authRoutes

---
+++ src/controllers/authController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { IUser } from '../model-typing/IUser';
+import User from '../models/User';
+import { UserTypes } from '../enums/UserTypes';
+
+
+export const registerUser = async (req: Request, res: Response) => {
+    const { firstName, lastName, password, type }: { firstName: string, lastName: string, password: string, type: UserTypes } = req.body;
+    console.log(firstName, lastName, type);
+    const user: IUser = new User({ firstName, lastName, password, type });
+    try {
+        await user.save();
+        res.status(200).send({
+            message: "User Created Successfully",
+            success: true,
+        });
+        return;
+    } catch (error) {
+        console.log("Error while Creating the user", error)
+        res.status(500).send({
+            message: "Internal Server Error",
+            success: false,
+        })
+    }
+
+
+}
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,33 +1,10 @@
 import express, { Request, Response } from 'express';
-import User from '../models/User';
-import { UserTypes } from '../enums/UserTypes';
-import { IUser } from '../model-typing/IUser';
-import { hashPassword } from '../helpers/hashing';
+import { registerUser } from '../controllers/authController'
 
 
 const router = express.Router();
 
-router.post('/register', async (req: Request, res: Response) => {
-    const { firstName, lastName, password, type }: { firstName: string, lastName: string, password: string, type: UserTypes } = req.body;
-    console.log(firstName, lastName, type);
-    const user: IUser = new User({ firstName, lastName, password, type });
-    try {
-        await user.save();
-        res.status(200).send({
-            message: "User Created Successfully",
-            success: true,
-        });
-        return;
-    } catch (error) {
-        console.log("Error while Creating the user", error)
-        res.status(500).send({
-            message: "Internal Server Error",
-            success: false,
-        })
-    }
-
-
-});
+router.post('/register', registerUser);
 
 router.post('/login', (req: Request, res: Response) => {
 
